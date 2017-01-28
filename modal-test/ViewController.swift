ca2367b69a8157209ccd82bcc0c7d21c8d624953
Drew Westcott <code@drewwestcott.co.uk>
2017-01-28 15:51:46 +0000

    Initial Commit

---
+++ modal-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modal-test
+//
+//  Created by Drew Westcott on 28/01/2017.
+//  Copyright © 2017 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
